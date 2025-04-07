5724f88634be52f05253541e534f51c9087f61a7
Bae JaeWon <60336857+hohre12@users.noreply.github.com>
2025-04-08 01:41:54 +0900

    UPDATE] layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,48 @@
 import './App.css'
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom'
+import GlobalNavigationBar from './components/globalNavigationBar/GlobalNavigationBar'
+import SideNavigationBar from './components/sideNavigationBar/SideNavigationBar'
+import { ReactNode } from 'react'
+import styled from 'styled-components'
+import TodoList from './pages/Todo/List'
 
+interface PrivateRouteProps {
+  children: ReactNode
+}
+
+const WrapperRoute = ({ children }: PrivateRouteProps) => {
+  return <RootWrapper>{children}</RootWrapper>
+}
+
+// 버튼, 인풋, 컨펌, 토스트, 테이블로우, 모달
 function App() {
-
   return (
     <div>
-        <Routes>
-            <Route path="/todo">
-                <Route
-                    index
-                    element={<></>}
-                />
-            </Route>
-            <Route
-            path="*"
+      <GlobalNavigationBar></GlobalNavigationBar>
+      <SideNavigationBar></SideNavigationBar>
+      <Routes>
+        <Route path="/todo">
+          <Route
+            index
             element={
-                <Navigate
-                replace
-                to="/todo"
-                />
+              <WrapperRoute>
+                <TodoList></TodoList>
+              </WrapperRoute>
             }
-            />
-        </Routes>
+          />
+        </Route>
+        <Route path="*" element={<Navigate replace to="/todo" />} />
+      </Routes>
     </div>
   )
 }
 
 export default App
+
+const RootWrapper = styled.div`
+  width: calc(100% - 100px);
+  height: calc(100% - 60px);
+  position: fixed;
+  left: 100px;
+  top: 60px;
+`
